6488f41419fbc66ab5c1cb7561671e68613cdcd2
auba86 <audrius.bareisis@gmail.com>
2020-10-08 22:45:16 +0300

    Initial commit

---
+++ src/main/java/net/javaguides/springboot/SpringbootThymeleafCrudWebAppApplication.java
@@ -0,0 +1,15 @@
+package net.javaguides.springboot;
+
+import model.Employee;
+import org.junit.runner.RunWith;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafCrudWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeleafCrudWebAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/service/EmployeeServiceImpl.java
@@ -0,0 +1,20 @@
+package service;
+
+import model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repository.EmployeeRepository;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
